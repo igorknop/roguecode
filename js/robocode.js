0f114aad0713e642941a0945c145679a93359e40
Igor Knop <igorknop@gmail.com>
2017-05-12 23:43:32 -0300

    Add timer functions to  controll buttons

--- js/robocode.js
+++ js/robocode.js
@@ -1,22 +1,57 @@
 var robocode;
 var srcElement;
 
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function() {
   robocode = new RoboCode();
   robocode.init();
+  robocode.setStatus("Stopped");
+  robocode.draw();
+  var status = document.getElementsByTagName("span")[0];
+  document.getElementById("play").addEventListener("click", function() {
+    robocode.setStatus("Running");
+    robocode.timer = setInterval(function() {
+      robocode.step();
+    }, 1000);
+  });
+  document.getElementById("pause").addEventListener("click", function() {
+    robocode.setStatus("Paused");
+    clearInterval(robocode.timer);
+  });
+  document.getElementById("stop").addEventListener("click", function() {
+    robocode.setStatus("Stopped");
+    clearInterval(robocode.timer);
+  });
 }, false);
 
-function RoboCode(){
+function RoboCode() {
+  this.timer = null;
+  this.status = "Stopped";
   this.lines = [];
   this.commands = [];
   this.srcElement = null;
 }
 
-RoboCode.prototype.init = function () {
+RoboCode.prototype.init = function() {
   this.lines = document.querySelectorAll("#mainProg li");
   this.commands = document.querySelectorAll("#pallete li");
+  this.canvas = document.querySelectorAll("canvas")[0];
+  this.ctx = this.canvas.getContext("2d");
+  this.map = new Map();
+  this.map.load([
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ]);
 
-  this.lines.forEach(function(line){
+  this.lines.forEach(function(line) {
     line.addEventListener('dragstart', handleDragStart, false);
     line.addEventListener('dragenter', handleDragEnter, false);
     line.addEventListener('dragover', handleDragOver, false);
@@ -24,7 +59,7 @@ RoboCode.prototype.init = function () {
     line.addEventListener('drop', handleDropInLine, false);
     line.addEventListener('dragend', handleDragEnd, false);
   });
-  this.commands.forEach(function(comm){
+  this.commands.forEach(function(comm) {
     comm.addEventListener('dragstart', handleDragStart, false);
     comm.addEventListener('dragenter', handleDragEnter, false);
     comm.addEventListener('dragover', handleDragOver, false);
@@ -35,46 +70,63 @@ RoboCode.prototype.init = function () {
 
 };
 
-handleDragStart = function(e){
+RoboCode.prototype.draw = function() {
+  this.fillStyle = "black";
+  this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+  this.map.draw(this.ctx);
+};
+
+RoboCode.prototype.step = function(t) {
+  dt = t - this.previous;
+  this.draw();
+  this.previous = t;
+};
+
+RoboCode.prototype.setStatus = function(newStatus){
+  this.status = newStatus;
+  document.getElementById("status").innerHTML = this.status;
+}
+
+handleDragStart = function(e) {
   this.style.opacity = 0.4;
   srcElement = this;
   e.dataTransfer.effectAllowed = 'move';
   e.dataTransfer.setData('text/html', this.innerHTML);
 }
-handleDragOver = function(e){
+handleDragOver = function(e) {
   if (e.preventDefault) e.preventDefault();
   e.dataTransfer.dropEffect = 'move';
   return false;
 }
-handleDragEnter = function(e){
+handleDragEnter = function(e) {
   this.classList.add('over');
 }
-handleDragLeave = function(e){
+handleDragLeave = function(e) {
   this.classList.remove('over');
 }
-handleDrop = function(e){
-  if(this.stopPropagation) this.stopPropagation();
-  if(srcElement!==this){
+handleDrop = function(e) {
+  if (this.stopPropagation) this.stopPropagation();
+  if (srcElement !== this) {
 
     srcElement.innerHTML = this.innerHTML;
     this.innerHTML = e.dataTransfer.getData("text/html");
   }
   return false;
 }
-handleDropInLine = function(e){
-  if(this.stopPropagation) this.stopPropagation();
-  if(srcElement!==this){
+handleDropInLine = function(e) {
+  if (this.stopPropagation) this.stopPropagation();
+  if (srcElement !== this) {
     this.innerHTML = e.dataTransfer.getData("text/html");
   }
   return false;
 }
 
-handleDragEnd = function(e){
-  document.querySelectorAll("#mainProg li").forEach(function(comm){
+handleDragEnd = function(e) {
+  document.querySelectorAll("#mainProg li").forEach(function(comm) {
     comm.classList.remove('over');
     comm.style.opacity = 1;
   });
-  document.querySelectorAll("#pallete li").forEach(function(comm){
+  document.querySelectorAll("#pallete li").forEach(function(comm) {
     comm.classList.remove('over');
     comm.style.opacity = 1;
   });
